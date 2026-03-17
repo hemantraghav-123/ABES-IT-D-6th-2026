8e1b61d6ddbeab50c1364cb16b4142401541f8e1
Hemant Raghav <hr246740@gmail.com>
2026-03-17 12:52:36 +0530

    http server with login/register api

---
+++ day_10/index.js
@@ -0,0 +1,33 @@
+import http from "http";
+import { userLogin } from "./login.js";
+import { register } from "./register.js";
+const PORT = 8800;
+const server = http.createServer((req, res) => {
+    if (req.url === "/login" && req.method === "POST") {
+        let body = "";
+        req.on("data", (dataChunk) => {
+            body += dataChunk.toString();
+        })
+        req.on("end", async () => {
+            const userDetails = JSON.parse(body);
+            const response = await userLogin(userDetails, "./users.json");
+            res.writeHead(200, { "Contant-type": "application/json" });
+            res.end(JSON.stringify(response));
+        })
+    } else if (req.url === "/register", req.method === "POST") {
+        let body = "";
+        req.on("data", (dataChunk) => {
+            body += dataChunk.toString();
+        })
+        req.on("end", async () => {
+            const userDetails = JSON.parse(body);
+            const response = await register(userDetails, "./users.json");
+            res.writeHead(200, { "Contant-type": "application/json" });
+            res.end(JSON.stringify(response));
+        })
+    }
+    else {
+        res.end("Server is live");
+    }
+})
+server.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
