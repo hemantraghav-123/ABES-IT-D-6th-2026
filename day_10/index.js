5d8aa21da9946291945b6655562f831aa91d047e
Hemant Raghav <hr246740@gmail.com>
2026-03-18 10:19:24 +0530

    18-03-2026

--- day_10/index.js
+++ day_10/index.js
@@ -1,6 +1,8 @@
 import http from "http";
 import { userLogin } from "./login.js";
 import { register } from "./register.js";
+import { changePassword } from "./changePassword.js";
+import {deleteUser} from "./deleteUser.js"
 const PORT = 8800;
 const server = http.createServer((req, res) => {
     if (req.url === "/login" && req.method === "POST") {
@@ -9,12 +11,12 @@ const server = http.createServer((req, res) => {
             body += dataChunk.toString();
         })
         req.on("end", async () => {
-            const userDetails = JSON.parse(body);
+            const userDetails = JSON.parse(body); //obj
             const response = await userLogin(userDetails, "./users.json");
             res.writeHead(200, { "Contant-type": "application/json" });
             res.end(JSON.stringify(response));
         })
-    } else if (req.url === "/register", req.method === "POST") {
+    } else if (req.url === "/register" && req.method === "POST") {
         let body = "";
         req.on("data", (dataChunk) => {
             body += dataChunk.toString();
@@ -25,8 +27,29 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { "Contant-type": "application/json" });
             res.end(JSON.stringify(response));
         })
-    }
-    else {
+    } else if (req.url === "/change-password" && req.method === "POST") {
+        let body = "";
+        req.on("data", (dataChunk) => {
+            body += dataChunk.toString();
+        })
+        req.on("end", async () => {
+            const userDetails = JSON.parse(body);
+            const response = await changePassword(userDetails, "./users.json");
+            res.writeHead(200, { "Contant-type": "application/json" });
+            res.end(JSON.stringify(response));
+        })
+    } else if (req.url === "/delete-user" && req.method === "POST") {
+        let body = "";
+        req.on("data", (dataChunk) => {
+            body += dataChunk.toString();
+        })
+        req.on("end", async () => {
+            const userDetails = JSON.parse(body);
+            const response = await deleteUser(userDetails, "./users.json");
+            res.writeHead(200, { "Contant-type": "application/json" });
+            res.end(JSON.stringify(response));
+        })
+    } else {
         res.end("Server is live");
     }
 })
